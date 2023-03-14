80ea1c9a08ebd4bd55d47895703d78a42584dbf4
Paolacocc <coccettipaola@gmail.com>
2023-03-14 15:35:52 +0100

    snack 1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+
+
+for ( let i = 1; i <= 10; i++){
+    let userNum = prompt("inserisci un numero");
+    
+    console.log(userNum);
+}
